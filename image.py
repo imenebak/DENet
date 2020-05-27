20b6440355ec77e5dfc8093f8e167b6180538f39
Imene <34316110+imenebak@users.noreply.github.com>
2020-05-27 10:49:06 +0200

    Update image.py

--- image.py
+++ image.py
@@ -11,31 +11,40 @@ def load_data(img_path,train = True):
     img = Image.open(img_path).convert('RGB')
     gt_file = h5py.File(gt_path)
     target = np.asarray(gt_file['density'])
+    print(target)
     scale=1
     scale_out=1
     real_scale=scale/scale_out
     change=np.square(scale / scale_out)
 
     # finish
-    if True:
-        crop_size = (img.size[0] / 8 * 8, img.size[1] / 8 * 8)
-        dx=0
-        dy=0
+    if False:
+        crop_size = (img.size[0]/2,img.size[1]/2)
+        if random.randint(0,9)<= -1:
+            
+            
+            dx = int(random.randint(0,1)*img.size[0]*1./2)
+            dy = int(random.randint(0,1)*img.size[1]*1./2)
+        else:
+            dx = int(random.random()*img.size[0]*1./2)
+            dy = int(random.random()*img.size[1]*1./2)
+        
+        
+        
         img = img.crop((dx,dy,crop_size[0]+dx,crop_size[1]+dy))
         target = target[dy:crop_size[1]+dy,dx:crop_size[0]+dx]
-        target = cv2.resize(target, (target.shape[1] / real_scale, target.shape[0] / real_scale),
-                            interpolation=cv2.INTER_CUBIC) * change
-        return img,target
-
-
-
-#
-    # target = cv2.resize(target, (target.shape[1] / scale, target.shape[0] / scale), interpolation=cv2.INTER_CUBIC)
-    # target = cv2.resize(target, (target.shape[1] * scale_out, target.shape[0] * scale_out), interpolation=cv2.INTER_CUBIC)
-    # target = target * change
-    # target = cv2.resize(target, (target.shape[1] / 8, target.shape[0] / 8), interpolation=cv2.INTER_CUBIC) * 64
-    # target = cv2.resize(target, (target.shape[1] / 4, target.shape[0] / 4), interpolation=cv2.INTER_CUBIC) * 16
-    # target = cv2.resize(target, (target.shape[1] / 2, target.shape[0] / 2), interpolation=cv2.INTER_CUBIC) * 4
-#
-#
-    return img, target
+        
+        
+        
+        
+        if random.random()>0.8:
+            target = np.fliplr(target)
+            img = img.transpose(Image.FLIP_LEFT_RIGHT)
+    
+    
+    
+    
+    target = cv2.resize(target,(target.shape[1]//8,target.shape[0]//8),interpolation = cv2.INTER_CUBIC)*64
+    
+    
+    return img,target
