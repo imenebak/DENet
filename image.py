a8cb2e4ca0786238aad3cbe217eff69579ff0b68
Imene <34316110+imenebak@users.noreply.github.com>
2020-05-27 09:05:06 +0200

    Update image.py

--- image.py
+++ image.py
@@ -7,7 +7,7 @@ from PIL import ImageStat
 import cv2
 
 def load_data(img_path,train = True):
-    gt_path = img_path.replace('.jpg','.h5').replace('images','ground_truth')
+    gt_path = img_path.replace('.jpg','.h5').replace('sequences','ground_truth')
     img = Image.open(img_path).convert('RGB')
     gt_file = h5py.File(gt_path)
     target = np.asarray(gt_file['density'])
@@ -19,19 +19,12 @@ def load_data(img_path,train = True):
     # finish
     if True:
         crop_size = (img.size[0] / 8 * 8, img.size[1] / 8 * 8)
-        # dx = int(random.random() * img.size[0] * 1 / 2)
-        # dy = int(random.random() * img.size[1] * 1 / 2)
         dx=0
         dy=0
         img = img.crop((dx,dy,crop_size[0]+dx,crop_size[1]+dy))
         target = target[dy:crop_size[1]+dy,dx:crop_size[0]+dx]
         target = cv2.resize(target, (target.shape[1] / real_scale, target.shape[0] / real_scale),
                             interpolation=cv2.INTER_CUBIC) * change
-        #
-        # print np.sum(target)
-        # target = cv2.resize(target, (target.shape[1] / scale, target.shape[0] / scale), interpolation=cv2.INTER_CUBIC)
-        # target = cv2.resize(target, (target.shape[1] * scale_out, target.shape[0] * scale_out), interpolation=cv2.INTER_CUBIC)
-        # target = target*change
         return img,target
 
 
